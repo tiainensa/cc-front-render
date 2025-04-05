650b58159dde87b19bca1f76cbbc81b6f61aadcd
tiainensa <144015802+tiainensa@users.noreply.github.com>
2025-04-05 20:31:09 +0300

    update sentiment analysis API endpoint to local server

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@ export default {
   methods: {
     async analyzeSentiment() {
       try {
-        const response = await fetch("https://cc-backend-git-cloud-computing-2025-course.2.rahtiapp.fi/sentiment", {
+        const response = await fetch("http://127.0.0.1:5000", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ text: this.userInput }),
